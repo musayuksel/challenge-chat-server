3608faae26ff04b6959dc88c8fc37f9be5f00141
musayuksel <musayuxel@gmail.com>
2021-11-04 18:11:03 +0000

    feat: Add example data and add get request for all messages

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+const data = require("./data/data.json");
+//send all messages
+app.get("/messages", (require, response) => {
+  response.send(data);
+});
 app.listen(PORT, () => {
   console.log(`Server working on ${PORT}`);
 });
